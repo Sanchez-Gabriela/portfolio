61834f67298f07543397c28deb8f2dab3554528e
Gabriela Sanchez <gaby.peta@gmail.com>
2020-05-04 15:43:07 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Paradigmshift', // <title>
+  manifestName: 'paradigmshift',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-paradigmshift/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Gabriela Sánchez',
+  subHeading: 'Full time Web Developer ',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+  ],
+  emailId: 'gaby.peta@gmail@gmail.com',
+  phone: '+43 676 488 05 98',
+  address: 'Vienna, Austria',
+};
---
+++ src/components/ContactForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+export default function ContactForm() {
+  return (
+    <>
+      <header>
+        <h2>Get in touch</h2>
+      </header>
+      <div className="content">
+        <p>
+          <strong>Please do not hesitate </strong>to contact me if you require further information.
+        </p>
+        <form>
+          <div className="fields">
+            <div className="field half">
+              <input type="text" name="name" id="name" placeholder="Name" />
+            </div>
+            <div className="field half">
+              <input type="email" name="email" id="email" placeholder="Email" />
+            </div>
+            <div className="field">
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Message"
+                rows="7"
+              />
+            </div>
+          </div>
+          <ul className="actions">
+            <li>
+              <input
+                type="submit"
+                value="Send Message"
+                className="button primary"
+              />
+            </li>
+          </ul>
+        </form>
+      </div>
+    </>
+  );
+}
